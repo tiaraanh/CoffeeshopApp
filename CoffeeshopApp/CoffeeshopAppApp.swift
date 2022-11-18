b38c33357873d9010f546575777f5a05983637e6
Tiara <tiaranurulh@gmail.com>
2022-11-18 20:03:15 +0700

    Initial Commit

---
+++ CoffeeshopApp/CoffeeshopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeshopAppApp.swift
+//  CoffeeshopApp
+//
+//  Created by Tiara H on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeshopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
